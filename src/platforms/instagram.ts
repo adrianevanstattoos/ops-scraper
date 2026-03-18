736009d1c20149b6e05156493b047d6ac557aadd
adrianevanstattoos <evansadrian801@gmail.com>
2026-03-18 13:28:06 -0600

    Update instagram.ts

--- src/platforms/instagram.ts
+++ src/platforms/instagram.ts
@@ -1,1 +1,8 @@
+import type { AccountJob, LatestContent } from "../lib/types";
 
+export async function getLatestInstagramContent(
+  account: AccountJob
+): Promise<LatestContent | null> {
+  void account;
+  return null;
+}
