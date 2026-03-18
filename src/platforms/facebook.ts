8bb8b92eb6f82016fcc5e4cb8c6c5973a08e5e34
adrianevanstattoos <evansadrian801@gmail.com>
2026-03-18 13:28:43 -0600

    Update facebook.ts

--- src/platforms/facebook.ts
+++ src/platforms/facebook.ts
@@ -1,1 +1,8 @@
+import type { AccountJob, LatestContent } from "../lib/types";
 
+export async function getLatestFacebookContent(
+  account: AccountJob
+): Promise<LatestContent | null> {
+  void account;
+  return null;
+}
